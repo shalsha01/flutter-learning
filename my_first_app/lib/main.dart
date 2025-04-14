a592a7edb89fd1956c6068ce71a75a6f630b51d1
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-14 21:06:14 +0300

    Enhance main application layout with gradient background and updated text

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,4 +1,21 @@
 import "package:flutter/material.dart";
-void main(){
-runApp(MaterialApp(home: Text("this is my home page ")));
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 36, 7, 122),
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.amber]
+                    )
+            ),
+          child: const Center(
+              child: Text("hello shahad")
+              ),
+        )
+        ),
+    ),
+  );
 }

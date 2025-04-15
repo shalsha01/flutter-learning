7b0d3c1d29fd3ade5b84a9775b3f156e068aa0e1
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-15 22:53:11 +0300

    Refactor main.dart to implement a gradient background and improve text styling; add GradientContainer widget for better structure

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -6,16 +6,45 @@ void main() {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 36, 7, 122),
         body: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.amber]
-                    )
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 13, 7, 134),
+                Color.fromARGB(243, 18, 0, 26),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
+          ),
           child: const Center(
-              child: Text("hello shahad")
-              ),
-        )
+            child: Text(
+              'Hello shahd',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
         ),
+      ),
     ),
   );
+}
+
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 13, 7, 134),
+            Color.fromARGB(243, 18, 0, 26),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+  }
 }

a4cd87c1c0ea32f2e40d0fb03dc94e2133c3f8de
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-16 17:57:46 +0300

    Refactor GradientContainer and styled_text to use variables for alignment and customize greeting message

--- my_first_app/lib/graident_container.dart
+++ my_first_app/lib/graident_container.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_first_app/styled_text.dart';
+var startalignment = Alignment.topLeft;
+var endalignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -14,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(113, 145, 139, 255),
             Color.fromARGB(161, 221, 216, 223),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin:startalignment,
+          end: endalignment,
         ),
       ),
       child: const Mystyle(),
--- my_first_app/lib/styled_text.dart
+++ my_first_app/lib/styled_text.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-
+var sayHello = "Hello,shahd!";
 class Mystyle extends StatelessWidget {
   const Mystyle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Hello, World!',
-      style: TextStyle(color: Colors.white, fontSize: 28),
+    return Text(
+      sayHello,
+      style: const TextStyle(color: Colors.white, fontSize: 28),
     );
   }
 }

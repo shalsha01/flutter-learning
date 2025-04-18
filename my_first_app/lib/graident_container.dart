b44d2eb794f3d3b49e75db9dc821f5310d96a2c6
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-18 19:03:50 +0300

    Refactor GradientContainer to use state management for dice rolling and update UI with a button

--- my_first_app/lib/graident_container.dart
+++ my_first_app/lib/graident_container.dart
@@ -1,33 +1,58 @@
-
 import 'package:flutter/material.dart';
+
 var startalignment = Alignment.topLeft;
 var endalignment = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key,required this.colors});
+class GradientContainer extends StatefulWidget {
+   const GradientContainer({super.key, required this.colors});
   final List<Color> colors;
-  
+
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+var activeDiceimage = "assets/images/dice-images/dice-1.png";
+
+   void rollDice() {
+    activeDiceimage= "assets/images/dice-images/dice-2.png";
+   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors:colors,
-          begin:startalignment,
+          colors: widget.colors,
+          begin: startalignment,
           end: endalignment,
         ),
       ),
       child: Center(
-        child:Column(children:[Image.asset(
-          "assets/images/dice-images/dice-3.png",
-          width: 200,
-          fit: BoxFit.cover,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Image.asset(
+                "assets/images/dice-images/dice-4.png",
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed:rollDice,
+              child: const Text(
+                "ROLL THE DICE",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
-        TextButton(onPressed:(){}, child:const Text("ROLL THE DICE",style: TextStyle(color: Colors.white),),),
-        ]),
       ),
     );
   }
 }
-
-

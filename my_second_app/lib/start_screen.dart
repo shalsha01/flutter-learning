e1c1a1257d7e74fa7b5999572a158ed14da679b2
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-05-30 23:21:49 +0300

    Add UI elements to StartScreen with logo, tagline, and start button

--- my_second_app/lib/start_screen.dart
+++ my_second_app/lib/start_screen.dart
@@ -7,7 +7,32 @@ class StartScreen extends StatelessWidget {
   Widget build(context) {
 	return Center(
     child: Column(
-      children: [],
+      mainAxisSize:MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+           width: 300,
+           color:const Color.fromARGB(255, 255, 255, 255),
+           ),
+         const SizedBox(height: 80),
+        Text("learn flutter the fun way!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 30),
+        OutlinedButton(
+          onPressed:(){},
+            style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,
+            side: const BorderSide(color: Colors.white, width: 2),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            textStyle: const TextStyle(fontSize: 18),
+            ),
+            child: const Text("Start Quiz"),)
+      ],
     ),
 	  );
 	

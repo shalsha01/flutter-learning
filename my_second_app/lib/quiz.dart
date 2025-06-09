655f6cb71d673b151d36e14c3364873da657fb3b
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-06-09 20:19:48 +0300

    Refactor Quiz state management to use string identifiers for active screens

--- my_second_app/lib/quiz.dart
+++ my_second_app/lib/quiz.dart
@@ -3,46 +3,40 @@ import 'package:my_second_app/start_screen.dart';
 import 'package:my_second_app/questions_secreen.dart';
 
 class Quiz extends StatefulWidget {
-   const Quiz({super.key});
+  const Quiz({super.key});
   @override
   State<Quiz> createState() {
-  return _QuizState();
+    return _QuizState();
   }
 }
 
-class  _QuizState extends State<Quiz> {
-  Widget ? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen= StartScreen(switchScreen);
-    super.initState();
-  }
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-      body: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xFF0A1C40),
-            Colors.black,
-          ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF0A1C40), Colors.black],
+            ),
+          ),
+          child:
+              activeScreen == 'start-screen'
+                  ? StartScreen(switchScreen)
+                  : const QuestionsScreen(),
         ),
       ),
-      child:activeScreen,
-      ),
-      ),
-    
-      );
+    );
   }
 }

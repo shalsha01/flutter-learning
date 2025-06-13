1c8b447777c672d2d95bb12c3d34fbe576a8ea19
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-06-13 16:34:57 +0300

    Enhance QuestionsScreen layout with question text and answer buttons

--- my_second_app/lib/questions_secreen.dart
+++ my_second_app/lib/questions_secreen.dart
@@ -3,15 +3,41 @@ import 'package:flutter/material.dart';
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
   @override
-State<QuestionsScreen> createState(){
+  State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
 
-
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-	return const Text('Questions Screen'); // Replace with your desired widget
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The questions .....' ,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, 
+          child: const Text('Answer 1')
+          ),
+           ElevatedButton(onPressed: () {}, 
+          child: const Text('Answer 2')
+          ),
+           ElevatedButton(onPressed: () {}, 
+          child: const Text('Answer 3')
+          ),
+           ElevatedButton(onPressed: () {}, 
+          child: const Text('Answer 4')
+          ),
+        ],
+      ),
+    ); // Replace with your desired widget
   }
 }
